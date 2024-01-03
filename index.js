d73e937d8e44651a2b48fa03551ef074a05c1055
Harshad 200 <bholo0090@gmail.com>
2024-01-03 22:45:19 +0530

    hello

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 console.log("harshad hello");
 
-
 const btn = document.getElementById("btn");
-btn.addEventListener("click",myjoke)
-
-const ApiKey = "HVn9TvKiSuyH9nx3+YW9+w==qjS0kYLQmVIqEzpe";
-const options = {
-           mathod: "GET",
-           headers: { 'X-Api-Key': ApiKey,
-           },
-};
+const joke_show = document.getElementById("joke");
 const ApiURL = "https://api.api-ninjas.com/v1/dadjokes?limit=1";
+btn.addEventListener("click", myjoke);
+// Api key:
+const ApiKey = "HVn9TvKiSuyH9nx3+YW9+w==qjS0kYLQmVIqEzpe";
 
+//
+const options = {
+  mathod: "GET",
+  headers: { "X-Api-Key": ApiKey },
+};
+// function for jokes show in html page.
 async function myjoke() {
-
-     const res = await fetch(ApiURL, options);
-     const data = await res.json();
-     console.log(data[0].joke);
-        const joke_daata = data[0].joke;
-        const joke_show  = document.getElementById("joke");
-        joke_show.innerHTML = joke_daata
-     
-    // console.log("hello");
+  joke_show.innerHTML = "Updating...";
+  btn.disabled = true;
+  btn.innerHTML = "Loading..."
+  const res = await fetch(ApiURL, options);
+  const data = await res.json();
+  btn.disabled = false;
+  btn.innerHTML = "Tell me a joke";
+//   console.log(data[0].joke);
+  const joke_daata = data[0].joke;
+  joke_show.innerHTML = joke_daata;
+  // console.log("hello");
 }
